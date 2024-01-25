48287d39a699247d07a9568b9f3e7595d445b1b5
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-01-26 00:00:00 +0100

    Initial Commit

---
+++ src/test/script/test_all.sh
@@ -0,0 +1,7 @@
+/bin/sh tests_lexico.sh
+/bin/sh tests_syntaxe.sh
+/bin/sh tests_context_invalid.sh
+/bin/sh tests_context_valids.sh
+/bin/sh tests_codegen.sh
+/bin/sh tests_gameboy.sh
+/bin/sh tests_gameboy_lib.sh
